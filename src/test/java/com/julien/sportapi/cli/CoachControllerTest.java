0bf2a63075548e85e14ed5fc28a4beb72cac9b85
corvaisier <julien.corvaisier.br@gmail.com>
2021-09-02 16:44:21 +0200

    add tests tu coach entity and refacto

---
+++ src/test/java/com/julien/sportapi/cli/CoachControllerTest.java
@@ -0,0 +1,27 @@
+package com.julien.sportapi.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CoachControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
---
+++ src/test/java/com/julien/sportapi/dao/Coach/CoachDaoInMemoryTest.java
@@ -0,0 +1,31 @@
+package com.julien.sportapi.dao.Coach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CoachDaoInMemoryTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
