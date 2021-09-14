7b57247597c6a23af9165af663236ec77aee44ca
corvaisier <julien.corvaisier.br@gmail.com>
2021-09-14 12:16:19 +0200

    generate tests

---
+++ src/test/java/com/julien/sportapi/cli/CoachControllerTest.java
@@ -0,0 +1,40 @@
+package com.julien.sportapi.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//TODO to implement !
+class CoachControllerTest {
+
+    @Test
+    void findLessonByDay() {
+    }
+
+    @Test
+    void findLessonByhour() {
+    }
+
+    @Test
+    void findLessonByCoachId() {
+    }
+
+    @Test
+    void addPersonToCoachList() {
+    }
+
+    @Test
+    void addLesson() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void testUpdate() {
+    }
+}
---
+++ src/test/java/com/julien/sportapi/service/CoachServiceTest.java
@@ -0,0 +1,45 @@
+package com.julien.sportapi.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//TODO to implement !
+class CoachServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findPerson() {
+    }
+
+    @Test
+    void attachPerson() {
+    }
+}
